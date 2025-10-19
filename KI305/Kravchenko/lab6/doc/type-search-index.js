2603ad5f68c907e5f514f113b717dce828d7b1b2
Vairyz <kpacad@gmail.com>
2025-10-19 15:31:40 +0300

    doc

---
+++ KI305/Kravchenko/lab6/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"KI305.Kravchenko.lab6","l":"Truck"},{"p":"KI305.Kravchenko.lab6","l":"TruckDriver"}];updateSearchResults();
